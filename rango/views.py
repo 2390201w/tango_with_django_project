368dfd5a8d394651a98d2081dbf4272e1a49f5ae
Yuhao Wu <2390201w@student.gla.ac.uk>
2020-02-13 02:51:14 +0000

    Chapter 8 Finished and Pass

--- rango/views.py
+++ rango/views.py
@@ -117,5 +117,4 @@ def add_page(request, category_name_slug):
             print(form.errors)
 
     context_dict = {'form': form, 'category': category}
-    print(context_dict)
     return render(request, 'rango/add_page.html', context=context_dict)
